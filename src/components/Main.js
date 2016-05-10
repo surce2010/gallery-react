edf6e4a9f6ad49e38f859f8399da08fdaf9bd304
truman <yangyangouc@gmail.com>
2016-05-10 13:02:39 +0800

    添加json-loader,添加图片资源

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,18 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+// 获取图片相关的数
+var imageDatas = require('../data/imagedata.json');
+// 利用自执行函数， 将图片名信息转成图片URL路径信息
+imageDatas = (function genImageURL(imageDatasArr) {
+    for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+        var singleImageData = imageDatasArr[i];
+        singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+        imageDatasArr[i] = singleImageData;
+    }
 
+    return imageDatasArr;
+})(imageDatas);
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
@@ -11,7 +22,7 @@ class AppComponent extends React.Component {
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <span>Hellow</span>
+        
       </div>
     );
   }
