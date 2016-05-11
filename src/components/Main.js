9c70a88dd7c4bc65848ea2f6b9a0516498582f2d
truman <yangyangouc@gmail.com>
2016-05-11 13:10:03 +0800

    添加样式表,修改render的结构

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,16 +14,19 @@ imageDatas = (function genImageURL(imageDatasArr) {
 
     return imageDatasArr;
 })(imageDatas);
-let yeomanImage = require('../images/yeoman.png');
-
+ 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        
-      </div>
+        <section className="stage" ref="stage">
+            <section className="img-sec">
+                
+            </section>
+            <nav className="controller-nav">
+             
+            </nav>
+        </section>
+       
     );
   }
 }
